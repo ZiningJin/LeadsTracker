37b0466e06976d6d7f3221c0a8ade59146d39c12
CharlieJin <72548752+ZiningJin@users.noreply.github.com>
2024-02-06 14:58:39 +0800

    Create data.js
    store links and other data for option values used in components

---
+++ client/src/data.js
@@ -0,0 +1,51 @@
+export const links = [
+    {
+        name: "About",
+        path: '/#about'
+    },
+    {
+        name: "Insert",
+        path: '/#insert'
+    },
+    {
+        name: "Track",
+        path:'/#track'
+    },
+    {
+        name:"Update",
+        path:'/#update'
+    },
+    {
+        name: "Download",
+        path: '/#download'
+    }
+];
+
+export const statusValues = [
+    "status1", "status2","status3"
+];
+
+export const wgamtValues = [
+    "Completes","Aftermarket","Other"
+];
+
+export const brandsValues = [
+    "brand1","brand2","brand3"
+];
+
+export const productcategoryValues = [
+    "Product1", "Product2", "Product3", "Product4"
+];
+
+export const leadsource1Values = [
+    "source1","source2","source3"
+];
+
+export const countryValues = ["China"];
+
+export const stateValues = [
+    "Beijing","Tianjin","Hebei","Shanxi","Nei Mongol","Liaoning", "Jilin", "Heilongjiang","Shanghai",
+    "Jiangsu","Zhejiang","Anhui","Fujian","Jiangxi","Shandong","Henan","Hubei","Hunan",
+    "Guangdong","Guangxi","Hainan","Chongqing","Sichuan","Guizhou","Yunnan","Xizang","Shaanxi","Gansu",
+    "Qinghai","Ningxia","Xinjiang","Chinese Taipei","Hong Kong","Macao"
+];
