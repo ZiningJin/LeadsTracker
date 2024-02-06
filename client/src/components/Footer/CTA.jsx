2f9affb32114c855f0b69da310e8a0e59e6c3bf6
CharlieJin <72548752+ZiningJin@users.noreply.github.com>
2024-02-06 15:03:36 +0800

    Create CTA.jsx

---
+++ client/src/components/Footer/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="" target='_ blank' rel="noopener noreferrer">Anonymous</a>
+        <a href="" target='_blank' rel="noopener noreferrer">Anonymous</a>
+    </div>
+  )
+}
+
+export default CTA
